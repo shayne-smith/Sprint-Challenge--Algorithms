e56f6f93c6c3ad0b4c0c657c140d2a04f2ee4508
seanmx96 <seanmx96@users.noreply.github.com>
2020-07-24 11:13:12 -0500

    Implemented function to count th recursively

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,25 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
+    # base case
+    if len(word) < 2:
+        return 0
     
-    pass
+    if f"{word[0]}{word[1]}" == "th":
+        return 1 + count_th(word[2:])
+
+    else:
+        return count_th(word[1:])
+
+
+if __name__ == '__main__':
+
+    word = ""
+    print("Count is: " + str(count_th(word)))
+
+    word = "abcthxyz"
+    print("Count is: " + str(count_th(word)))
+
+    word = "abcthefthghith"
+    print("Count is: " + str(count_th(word)))
+
